249319595b9c1352e219d4a348792a6fde88db56
Mariano Marco <mariano268@gmail.com>
2023-02-22 00:22:34 -0300

    Estructura del sitio creada. Clase 17

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const path = require('path')
 const app = express();
 
 app.listen (3000, () => { console.log ("Levantando un servidor con Express en puerto 3000")} );
 
-const publicPath = path.join(__dirname, "/public")
+const publicPath = path.join(__dirname, "/MercadoLiebre/public")
+
 app.use(express.static(publicPath))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'/views/home.html'))
+    res.sendFile(path.join(__dirname,'MercadoLiebre/views/home.html'))
 })
